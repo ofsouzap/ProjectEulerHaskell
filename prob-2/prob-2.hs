34c51850a6f7abeff74ffb0ac0c383bc96afe11d
Ofsouzap <ofsouzap@gmail.com>
2021-07-10 17:34:30 +0100

    Fixed problem 2 solution

--- prob-2/prob-2.hs
+++ prob-2/prob-2.hs
@@ -3,5 +3,9 @@ fib n
     | n == 1 = 1
     | otherwise = (fib (n-1)) + (fib (n-2))
 
+--The 33rd fib value would be greater than 4 million (pre-calculated)
+--Therefore, the 32nd fib value is the last lesser than 4 million
+maxFibI = 32
+
 main = do
-    print (sum [fib x | x <- [1..], fib x <= 4000000, x `mod` 2 == 0])
+    print (sum [fib x | x <- [1..maxFibI], (fib x) `mod` 2 == 0])
